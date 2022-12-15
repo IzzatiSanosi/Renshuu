e3b846320a96f87b9f5831fd372280cda6677d9e
IzzatiSanosi <sitinurizzati@graduate.utm.my>
2022-12-16 02:49:09 +0800

    First

---
+++ create_array.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+number_1 = np.array([2, 3, 2, 8, 9, 11]) #single dimension array
+number_2 = np.array([[1, 2, 3], [5, 10, 1]]) #multi-dimension array
+
+print(number_1)
+print(number_2)
